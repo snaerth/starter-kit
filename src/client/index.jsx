118cb6459ba72875127f21754bfa41e5616a8c22
Snær Seljan Þóroddsson <snaersth@islandsbanki.is>
2017-03-31 11:53:51 +0000

    react-redux-router added

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 import {Router, browserHistory} from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux'
 import match from 'react-router/lib/match';
 import routes from './routes';
 
 const preloadedState = window.__PRELOADED_STATE__;
 const store = configureStore(preloadedState);
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store);
 
 // Render the application
 const render = (Root, target = 'app') => {
@@ -31,7 +35,7 @@ const render = (Root, target = 'app') => {
         ReactDOM.render(
           <Root store={store}>
           <Router
-            history={browserHistory}
+            history={history}
             routes={routes}
             onUpdate={window.scrollTo(0, 0)}/>
         </Root>, document.getElementById(target));
--- src/client/reducers.js
+++ src/client/reducers.js
@@ -1,8 +1,20 @@
-import {combineReducers} from 'redux';
-import {reducer as form} from 'redux-form';
+import {
+  combineReducers
+} from 'redux';
+import {
+  reducer as form
+} from 'redux-form';
 import authReducer from './components/auth/reducer';
 import commonReducer from './common/reducer';
+import {
+  routerReducer
+} from 'react-router-redux'
 
-const rootReducer = combineReducers({common : commonReducer, form, auth: authReducer});
+const rootReducer = combineReducers({
+  common: commonReducer,
+  form,
+  auth: authReducer,
+  routing: routerReducer
+});
 
 export default rootReducer;
