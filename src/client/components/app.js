e749234556046e8d9d5b85b2dad61c3f4a7c99a3
Snær Seljan Þóroddsson <snaersth@islandsbanki.is>
2017-06-20 15:55:08 +0000

    Header titlte

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { CSSTransitionGroup } from 'react-transition-group';
 import AppLayout from './app-layout';
 import Header from './common/header';
 import routes from './../routes';
 
-const App = () => (
-  <AppLayout>
-    <Header name="Nafn á síðu" />
-    <CSSTransitionGroup
-      component="div"
-      transitionName="fadeInScale"
-      transitionEnterTimeout={700}
-      transitionLeaveTimeout={700}
-    >
-      {routes}
-    </CSSTransitionGroup>
-  </AppLayout>
-);
+class App extends Component {
+	render() {
+		return (
+			<AppLayout>
+				<Header name={'dfdf'} />
+				<CSSTransitionGroup
+					component="div"
+					transitionName="fadeInScale"
+					transitionEnterTimeout={700}
+					transitionLeaveTimeout={700}
+				>
+					{routes}
+				</CSSTransitionGroup>
+			</AppLayout>
+		);
+	}
+}
 
-export default App;
+function mapStateToProps(state) {
+	console.log(state);
+}
+
+export default connect(mapStateToProps)(App);
